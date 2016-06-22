80407d82ad6ca022cebeed14ae86f1676340100d
Tailor Vijay <tailor.vj@gmail.com>
2016-06-22 14:46:35 +0000

    modified server.js to grab mongo credentials from config.js and added config.js to gitignore

--- config.js
+++ config.js
@@ -1,8 +1,13 @@
 module.exports = {
   twitter: {
-    consumer_key: 'MY_CONSUMER_KEY',
-    consumer_secret: 'MY_CONSUMER_SECRET',
-    access_token_key: 'MY_ACCESS_TOKEN_KEY',
-    access_token_secret: 'MY_ACCESS_TOKEN_SECRET'
+    consumer_key: 'GzuatHJewEYNRwhytDmrKDk8c',
+    consumer_secret: '4Aj8w7fiJEjGAtswQWUNNGUt4LjDVaWLzndZjjg1JLWTJjoNFD',
+    access_token_key: '54329425-PR03P0ZvhFNBAKI0oecY8fOXU7BtyWijSonscqL9b',
+    access_token_secret: 'SFNgMXHKO9BkS5Aiil63yDlMMjXxWpUAwQlrnfbcK337G'
+  },
+  mongo: {
+    url: 'ds040309.mlab.com:40309/react-tweets',
+    user: 'dbuser',
+    password: 'dbpassword'
   }
 }
