fcfdb59ddb4536074d089e7db70406aa08beec48
Tailor Vijay <tailor.vj@gmail.com>
2016-06-22 15:13:53 +0000

    added config-example.js

---
+++ config-example.js
@@ -0,0 +1,13 @@
+module.exports = {
+  twitter: {
+    consumer_key: 'GzuatHJewEYNRwhytDmrKDk8c',
+    consumer_secret: '4Aj8w7fiJEjGAtswQWUNNGUt4LjDVaWLzndZjjg1JLWTJjoNFD',
+    access_token_key: '54329425-PR03P0ZvhFNBAKI0oecY8fOXU7BtyWijSonscqL9b',
+    access_token_secret: 'SFNgMXHKO9BkS5Aiil63yDlMMjXxWpUAwQlrnfbcK337G'
+  },
+  mongo: {
+    url: 'ds050607.mlab.com:40309/react-tweets',
+    user: 'dbuser',
+    password: 'dbpassword'
+  }
+}
