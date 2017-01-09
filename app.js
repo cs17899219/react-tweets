7ff4a61d33f7fa6d83013f18f9568ffc6a5dde66
CS <cs17899219@gmail.com>
2017-01-09 16:57:11 +0800

    Update runtime and fix minor warns

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 /** @jsx React.DOM */
 
-var React = require('react');
+var React = require('react'),
+    ReactDOM = require('react-dom');
 var TweetsApp = require('./components/TweetsApp.react');
 
 // Snag the initial state that was passed from the server side
 var initialState = JSON.parse(document.getElementById('initial-state').innerHTML)
 
 // Render the components, picking up where react left off on the server
-React.render(
+ReactDOM.render(
   <TweetsApp tweets={initialState}/>,
   document.getElementById('react-app')
 );
